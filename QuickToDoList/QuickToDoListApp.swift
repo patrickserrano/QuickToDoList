9d2a0cb346709aaa6fe382b83d07cdca937d9782
Patrick Serrano <hello@patrickserrano.com>
2022-02-19 14:36:35 -0500

    Initial Commit

---
+++ QuickToDoList/QuickToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickToDoListApp.swift
+//  QuickToDoList
+//
+//  Created by Patrick Serrano on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
